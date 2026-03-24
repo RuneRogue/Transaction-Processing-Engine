88c55fd9df4f955aa73148c5bea448557b5ea0f5
RuneRogue <priyankmakwana305@gmail.com>
2026-03-25 01:44:13 +0530

    Card Handler Completed

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/RuneRogue/Transaction-Processing-Engine
 
 go 1.25.5
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/cors v1.2.2
+	github.com/google/uuid v1.6.0
+)
--- handler/card_handler.go
+++ handler/card_handler.go
@@ -1,1 +1,45 @@
 package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/RuneRogue/Transaction-Processing-Engine/model"
+	"github.com/RuneRogue/Transaction-Processing-Engine/service"
+	"github.com/go-chi/chi"
+)
+
+type CardHandler struct {
+	cardService *service.CardService
+}
+
+type BalanceResponse struct {
+	Status  string `json:"status"`
+	Balance int64  `json:"balance,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+func NewCardHandler(cardService *service.CardService) *CardHandler {
+	return &CardHandler{cardService: cardService}
+}
+
+func (h *CardHandler) GetBalance(w http.ResponseWriter, r *http.Request) {
+	cardNumber := chi.URLParam(r, "cardNumber")
+
+	balance, err := h.cardService.GetBalance(cardNumber)
+	w.Header().Set("Content-Type", "application/json")
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(BalanceResponse{
+			Status:  model.TransactionStatusFailed,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	json.NewEncoder(w).Encode(BalanceResponse{
+		Status:  model.TransactionStatusSuccess,
+		Balance: balance,
+	})
+}
